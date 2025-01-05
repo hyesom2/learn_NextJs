cfc0777f1649ff1c74206556fcde728bf98458ce
kimHyeonJu <kimhyesom1202@gmail.com>
2025-01-05 23:01:24 +0900

    ✨ Basic of Layout in next.js

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,6 @@
-import Navigation from "../../components/navigation";
-
 export default function About() {
   return (
     <div>
-      <Navigation />
       <h1>Hello About Page!</h1>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
 // root segment : 유저가 root("/")에서 보이게 될 페이지
-import Navigation from "../components/navigation";
-
 export default function App() {
   return (
     <div>
-      <Navigation />
       <h1> Hello! Next.js!</h1>
     </div>
   )
